e538916e59a1c20fba7406abc1b884c25b228bd7
adsimicro@outlook.es <adsimicro@outlook.es>
2020-07-30 17:01:29 -0500

    Clase entidad

---
+++ WebHospital/WebHospital/Codigo/Entidad/clEntidadCita.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHospital.Codigo.Entidad
+{
+    public class clEntidadCita
+    {
+        public int Documento { get; set; }
+        public int Numero { get; set; }
+        public string Correo { get; set; }
+        public int idEps{ get; set; }
+        public int idDoctor { get; set; }
+        public string Fecha { get; set; }
+
+    }
+}

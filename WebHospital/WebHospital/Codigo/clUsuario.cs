1552b017aae6ccec910cbc2a6415324975355f9f
Johjan Jr Mendivelso <johjan99@gmail.com>
2020-07-30 13:39:48 -0500

    se creo la clase usuario

---
+++ WebHospital/WebHospital/Codigo/clUsuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHospital.Codigo
+{
+    public class clUsuario
+    {
+        public int mtdRegistrar(ClEntidadUsuario objUsuario)
+        {
+            string sqlInsert = "INSERT INTO Usuario(Nombre,Apellido,Documento,Usuario,Cntraseña,Genero)" +
+                "values('"+objUsuario.Nombre+ "','" + objUsuario.Apellido + "''" + objUsuario.Documento + "'" +
+                "'" + objUsuario.Usuario + "','" + objUsuario.Contraseña + "','" + objUsuario.Genero + "')";
+        }
+    }
+}

bd47f7d25fdfaa1d872fa64fedc8aa8679fd1259
Brayan Vargas <vargasalarcon2001@gmail.com>
2020-08-02 02:14:30 -0500

    clPersonalAdmi

---
+++ WebHospital/WebHospital/Codigo/Entidad/clEntidadPersonalAdministrativo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHospital.Codigo.Entidad
+{
+    public class clEntidadPersonalAdministrativo : ClEntidadUsuario
+    {
+        public string Rol { get; set; }
+        public int IdPersonalAdministrativo { get; set; }
+        public int IdRol { get; set; }
+    }
+}
---
+++ WebHospital/WebHospital/Codigo/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebHospital.Codigo
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            // Código que se ejecuta al iniciarse una nueva sesión
+            Session["usuario"] = "";
+            Session["rol"] = "";
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

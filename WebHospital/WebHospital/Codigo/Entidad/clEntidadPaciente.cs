8b117cedc665dc181ad32099fa850c5d58fa8093
Johjan Jr Mendivelso <johjan99@gmail.com>
2020-08-04 13:14:40 -0500

    entidad paciente

--- WebHospital/WebHospital/Codigo/Entidad/clEntidadPaciente.cs
+++ WebHospital/WebHospital/Codigo/Entidad/clEntidadPaciente.cs
@@ -5,14 +5,17 @@ using System.Web;
 
 namespace WebHospital.Codigo
 {
-    public class ClEntidadUsuario
+    public class ClEntidadPaciente
     {
-        public int IdUsuario { get; set; }
+        public int IdPaciente { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Documento { get; set; }
         public string email { get; set; }
-        public string password { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public DateTime FechaNAcimiento { get; set; }
+        public string Password { get; set; }
         public string Genero { get; set; }
         public int IdEps { get; set; }
 

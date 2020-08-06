301b2ac35e801d4934dc4c048f6ed9eb5cbd4129
adsimicro@outlook.es <adsimicro@outlook.es>
2020-08-06 16:31:09 -0500

    sd

--- WebHospital/WebHospital/Vista/Template/CITAS.aspx.cs
+++ WebHospital/WebHospital/Vista/Template/CITAS.aspx.cs
@@ -58,9 +58,18 @@ namespace WebHospital.Vista.Template
 
         protected void RegistroCita(object sender, EventArgs e)
         {
-            clEntidadCita objCita = new clEntidadCita();
-            objCita.Documento = TxtNombre.Text;
+            clEntidadCita objECita = new clEntidadCita();
+            //objECita.Documento = TxtNombre.Text;
+            objECita.FechaHIngreso = DateTime.Parse(TxtCita.Text);
+            objECita.IdMedico= int.Parse(dropEspecialista.Text);
+            objECita.IdPaciente = int.Parse(TxtNombre.Text);
+            objECita.IdEspecialidad = int.Parse(dropEspecialidad.Text);
             
+
+
+            clCita objCita = new clCita();
+            int result = objCita.mtdRegistrarCita(objECita);
+
         }
     }
 }
--- WebHospital/WebHospital/Vista/Template/CITAS.aspx.designer.cs
+++ WebHospital/WebHospital/Vista/Template/CITAS.aspx.designer.cs
@@ -51,12 +51,12 @@ namespace WebHospital.Vista.Template
         protected global::System.Web.UI.WebControls.DropDownList dropEspecialista;
 
         /// <summary>
-        /// Control TextBox1.
+        /// Control TxtCita.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        protected global::System.Web.UI.WebControls.TextBox TxtCita;
     }
 }

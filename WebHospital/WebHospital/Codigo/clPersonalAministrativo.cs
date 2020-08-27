490451fe0005b4d2faa24928a5109d2c8b00e27a
Brayan Vargas <vargasalarcon2001@gmail.com>
2020-08-27 16:52:53 -0500

    .

--- WebHospital/WebHospital/Codigo/clPersonalAministrativo.cs
+++ WebHospital/WebHospital/Codigo/clPersonalAministrativo.cs
@@ -11,9 +11,14 @@ namespace WebHospital.Codigo
 
         public int mtdRegistroPersonalAdministrativo(clEntidadPersonalAdministrativo objEPersonalAdmin)
         {
+            string sqlInsert = "Insert Into PersonalAdministrativo (Nombre,Apellido,Documento,Telefono,email,Direccion,Password,IdRol)" +
+                "Values ('" + objEPersonalAdmin.Nombre + "','" + objEPersonalAdmin.Apellido + "','" + objEPersonalAdmin.Documento + "'," +
+                "'" + objEPersonalAdmin.Telefono + "','" + objEPersonalAdmin.Direccion + "','" + objEPersonalAdmin.Password + "','" + objEPersonalAdmin.IdRol + "')";
 
+            clAdminSQL objSQL = new clAdminSQL();
+            int Resultado = objSQL.mtdConectado(sqlInsert);
 
-            return 0;
+            return Resultado;
         }
     }
 }

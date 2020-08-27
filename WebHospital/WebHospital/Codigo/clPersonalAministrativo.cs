ac8aaff3525414283883b2cb7704db1ff1b347e9
Brayan Vargas <vargasalarcon2001@gmail.com>
2020-08-27 14:35:31 -0500

    ,

---
+++ WebHospital/WebHospital/Codigo/clPersonalAministrativo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebHospital.Codigo.Entidad;
+
+namespace WebHospital.Codigo
+{
+    public class clPersonalAministrativo
+    {
+
+        public int mtdRegistroPersonalAdministrativo(clEntidadPersonalAdministrativo objEPersonalAdmin)
+        {
+
+
+            return 0;
+        }
+    }
+}

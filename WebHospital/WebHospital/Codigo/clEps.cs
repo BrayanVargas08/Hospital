67c76b4d9ccfb157066c0fe2d56f4f87ce703d66
Alejandro5207 <67340799+Alejandro5207@users.noreply.github.com>
2020-08-30 21:58:35 -0500

    cambios segundo urgencia

--- WebHospital/WebHospital/Codigo/clEps.cs
+++ WebHospital/WebHospital/Codigo/clEps.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using WebHospital.Codigo.Entidad;
@@ -16,6 +17,29 @@ namespace WebHospital.Codigo
         clAdminSQL objSQL = new clAdminSQL();
         int result = objSQL.mtdConectado(sqlInsert);
             return result;
+        }
+
+        public List<clEntidadEps> mtdListar()
+        {
+            string sql = "select * from Eps";
+            clAdminSQL objSQL = new clAdminSQL();
+            DataTable tblEps = new DataTable();
+            tblEps = objSQL.mtdDesconectado(sql);
+
+            List<clEntidadEps> listaEps = new List<clEntidadEps>();
+
+            for (int i = 0; i < tblEps.Rows.Count; i++)
+            {
+                clEntidadEps objEEps = new clEntidadEps();
+                objEEps.IdEps = int.Parse(tblEps.Rows[i][0].ToString());
+                objEEps.Nombre = tblEps.Rows[i][1].ToString();
+                objEEps.Ciudad = tblEps.Rows[i][2].ToString();
+              
+                listaEps.Add(objEEps);
+
+            }
+            return listaEps;
+
         }
 
     }

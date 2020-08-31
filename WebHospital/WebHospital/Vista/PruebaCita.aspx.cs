cbb18b1b325da4dc9e622e2731a69441733c8bcb
adsimicro@outlook.es <adsimicro@outlook.es>
2020-08-31 13:40:46 -0500

    AAA

---
+++ WebHospital/WebHospital/Vista/PruebaCita.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebHospital.Vista
+{
+    public partial class PruebaCita : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                IniciarllenadodeDropDown();
+
+            }
+
+
+        }
+        private void IniciarllenadodeDropDown()
+        {
+
+            dropEspecialidadP.DataSource = Consultar("SELECT * FROM Especialidad");
+            dropEspecialidadP.DataTextField = "Especialidad";
+            dropEspecialidadP.DataValueField = "IdEspecialidad";
+            dropEspecialidadP.DataBind();
+            dropEspecialidadP.Items.Insert(0, new ListItem("[Seleccionar] ", "0"));
+            dropEspecialistaP.Items.Insert(0, new ListItem("[Seleccionar] ", "0"));
+        }
+        protected void EspecialidadSeleccionada(object sender, EventArgs e)
+        {
+            int IdEspecialidad = Convert.ToInt32(dropEspecialidadP.SelectedValue);
+            dropEspecialistaP.DataSource = Consultar("SELECT * FROM Medico WHERE IdEspecialidad=" + IdEspecialidad);
+            dropEspecialistaP.DataTextField = "Nombre";
+            dropEspecialistaP.DataValueField = "IdMedico";
+            dropEspecialistaP.DataBind();
+            dropEspecialistaP.Items.Insert(0, new ListItem("[Seleccionar] ", "0"));
+        }
+
+
+        public DataSet Consultar(string strSQL)
+        {
+            string strconn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\dbHospitalPaz.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(strconn);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(strSQL, con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            con.Close();
+            return ds;
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WebHospital/WebHospital/Vista/PruebaCita.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace WebHospital.Vista
+{
+
+
+    public partial class PruebaCita
+    {
+
+        /// <summary>
+        /// Control txtDocumentoP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDocumentoP;
+
+        /// <summary>
+        /// Control dropEspecialidadP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList dropEspecialidadP;
+
+        /// <summary>
+        /// Control dropEspecialistaP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList dropEspecialistaP;
+
+        /// <summary>
+        /// Control TxtFechaP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtFechaP;
+
+        /// <summary>
+        /// Control DropDownList3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList3;
+
+        /// <summary>
+        /// Control DropDownList4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList4;
+
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+    }
+}

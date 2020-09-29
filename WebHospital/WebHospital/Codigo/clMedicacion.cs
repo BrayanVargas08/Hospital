3e8ba24d408a06ad5f8892f3645054a1c7392564
Johjan Jr Mendivelso <johjan99@gmail.com>
2020-09-29 15:05:43 -0500

    medicacion

---
+++ WebHospital/WebHospital/Codigo/clMedicacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebHospital.Codigo.Entidad;
+
+namespace WebHospital.Codigo
+{
+    public class clMedicacion
+    {
+        public int mtdRegistrar(clEntidadMedicacion objMedicacion)
+        {
+
+            string sqlInsert = "INSERT INTO Hospitalizacion(HoraMedicacion,Cantidad,MetodoAplicacion,IdProcesoGeneral,IdMedicamento,IdProcedimiento)" +
+               "values('" + objMedicacion.HoraMedicacion + "','" + objMedicacion.Cantidad + "','" + objMedicacion.MetodoAplicacion + "','" + objMedicacion.IdProcesoGeneral + "','" + objMedicacion.IdMedicamento + "','" + objMedicacion.IdProcedimiento + "')";
+
+            clAdminSQL objSQL = new clAdminSQL();
+            int result = objSQL.mtdConectado(sqlInsert);
+            return result;
+
+
+        }
+    }
+}

8ec45b3b11bf40950cfb885ad2d433f156ff8a7e
Johjan Jr Mendivelso <johjan99@gmail.com>
2020-09-28 23:38:03 -0500

    master

--- WebHospital/WebHospital/Vista/MasterUsuario.Master.cs
+++ WebHospital/WebHospital/Vista/MasterUsuario.Master.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebHospital.Codigo;
+using WebHospital.Codigo.Entidad;
 
 namespace WebHospital.Vista
 {
@@ -11,10 +13,43 @@ namespace WebHospital.Vista
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                List<clEntidadEspecialidad> listaEspecialidad = new List<clEntidadEspecialidad>();
+                clEspecialidad objEspecialidad = new clEspecialidad();
+                listaEspecialidad = objEspecialidad.mtdListarEspecialidad();
+
+                dropEspecialidad.DataSource = listaEspecialidad;
+                dropEspecialidad.DataTextField = "Especialidad";
+                dropEspecialidad.DataValueField = "IdEspecialidad";
+                DataBind();
+
+            }
 
 
-            Response.Write("<script>alert('"+Session["usuario"]+".');</script>");
-            //lblUsuario.Text = Session["usuario"].ToString();
+        }
+        protected void EspecialidadSeleccionada(object sender, EventArgs e)
+        {
+            List<clEntidadMedico> listaMedicos = new List<clEntidadMedico>();
+            clMedico objMedico = new clMedico();
+            int IdEspecialista = int.Parse(dropEspecialidad.SelectedValue.ToString());
+            listaMedicos = objMedico.mtdListar(IdEspecialista);
+
+            dropEspecialista.DataSource = listaMedicos;
+            dropEspecialista.DataTextField = "Nombre";
+            dropEspecialista.DataValueField = "IdMedico";
+            dropEspecialista.DataBind();
+        }
+        protected void btnRegistrarCita_Click(object sender, EventArgs e)
+        {
+            clEntidadCita objECita = new clEntidadCita();
+            objECita.Documento = TxtDocumento.Text;
+            objECita.FechaHIngreso = DateTime.Parse(txtF.Text);
+            objECita.IdMedico = int.Parse(dropEspecialista.SelectedValue.ToString());
+            objECita.IdEspecialidad = int.Parse(dropEspecialidad.SelectedValue.ToString());
+            clCita objCita = new clCita();
+            int result = objCita.mtdRegistrarCita(objECita);
+
         }
     }
 }
--- WebHospital/WebHospital/Vista/MasterUsuario.Master.designer.cs
+++ WebHospital/WebHospital/Vista/MasterUsuario.Master.designer.cs
@@ -24,6 +24,51 @@ namespace WebHospital.Vista
         protected global::System.Web.UI.WebControls.Label lblUsuario;
 
         /// <summary>
+        /// Control TxtDocumento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtDocumento;
+
+        /// <summary>
+        /// Control dropEspecialidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList dropEspecialidad;
+
+        /// <summary>
+        /// Control dropEspecialista.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList dropEspecialista;
+
+        /// <summary>
+        /// Control txtF.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtF;
+
+        /// <summary>
+        /// Control btnRegistrarCita.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRegistrarCita;
+
+        /// <summary>
         /// Control ContentPlaceHolder1.
         /// </summary>
         /// <remarks>
